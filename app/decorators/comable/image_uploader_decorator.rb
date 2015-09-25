a3c7221b449e0edffe5217b1127adde98779ba39
YOSHIDA Hiroki <hyoshida@appirits.com>
2015-09-25 13:27:18 +0900

    Add Comable namespace to ImageUploader

--- app/decorators/comable/image_uploader_decorator.rb
+++ app/decorators/comable/image_uploader_decorator.rb
@@ -1,5 +1,4 @@
-# TODO: ImageUploader => Comable::ImageUploader
-ImageUploader.class_eval do
+Comable::ImageUploader.class_eval do
   # OVERRIDE: store_dir with tenant
   def store_dir
     "uploads/#{::Apartment::Tenant.current}/#{model.class.to_s.underscore}/#{model.id}"
--- test/decorators/comable/image_uploader_decorator_test.rb
+++ test/decorators/comable/image_uploader_decorator_test.rb
@@ -1,9 +1,9 @@
 require 'minitest_helper'
 
-class ImageUploaderTest < ActiveSupport::TestCase
+class Comable::ImageUploaderTest < ActiveSupport::TestCase
   test '#store_dir should returns the directory path for current tenant' do
     id = 1
     image = Comable::Image.new(id: id)
-    assert_equal ImageUploader.new(image).store_dir, "uploads/#{::Apartment::Tenant.current}/comable/image/#{id}"
+    assert_equal Comable::ImageUploader.new(image).store_dir, "uploads/#{::Apartment::Tenant.current}/comable/image/#{id}"
   end
 end
